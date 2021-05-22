d2c4bc290454a89b3be93951f9b23b98e7e2f7e6
radja iqbal <sanusiiqbal36@gmail.com>
2021-05-22 16:17:47 +0700

    cors 3

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ use Illuminate\Http\Resources\Json\ResourceResponse;
 //     return $request->user();
 // });
 /// login
-header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Origin', 'http://natunasandmine.com');
 Route::post('/login', [LoginController::class,'login']);
 // daftar pegawai
 
